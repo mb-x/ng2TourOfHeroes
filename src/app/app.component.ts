35e9bcbde9ada7517a8789ad58f8780129016ea5
ubuntu-orca <webmasterbengrich@gmail.com>
2017-03-10 19:19:44 +0000

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Hero } from './hero';
-
-import { HeroService } from './hero.service';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  styleUrls: ['./app.component.css']
 
 })
 
-export class AppComponent implements OnInit {
-  abstract;
+export class AppComponent {
 
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { }
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-  onSelect(hero: Hero): void{
-    console.log(hero);
-    this.selectedHero = hero;
-  }
 
 }
